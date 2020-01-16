440cd879e5181a172d77d46a953af0f5700b5ecb
kamo731 <kamok0731@gmail.com>
2020-01-16 19:28:27 +0900

    [add] bashMain, log

--- main.m
+++ main.m
@@ -1,3 +1,5 @@
+function main( seed, music_num, nb )
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Sample program for blind source separation using multichannel           %
 % nonnegative matrix factorization (multichannel NMF)                     %
@@ -13,23 +15,22 @@
 % See also:                                                               %
 % http://d-kitamura.net                                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-clear;
-close all;
+fprintf("music %s seed %s nb %s \n", num2str(music_num), num2str(seed), num2str(nb));
+% clear;
+% close all;
 addpath('./bss_eval'); % BSS eval is shared under GPLv3 license
-
-root_dir = "./experiment/test/";
-music_num = 1;
+addpath('./lib');
+root_dir = "./experiment/exp10_MNMF/";
 
 % Parameters
-seed = 1; % pseudo random seed
+% seed = 1; % pseudo random seed
 refMic = 1; % reference mic for performance evaluation using bss_eval_sources
 fsResample = 16000; % resampling frequency [Hz]
 ns = 2; % number of sources
-fftSize = 4096; % window length in STFT [points]
-shiftSize = 2048; % shift length in STFT [points]
-nb = 20; % number of NMF bases for all sources (total bases)
-it = 300; % number of iterations (define by checking convergence behavior with drawConv=true)
+fftSize = 1024; % window length in STFT [points]
+shiftSize = 256; % shift length in STFT [points]
+% nb = 20; % number of NMF bases for all sources (total bases)
+it = 6000; % number of iterations (define by checking convergence behavior with drawConv=true)
 drawConv = false; % true or false (true: plot cost function values in each iteration and show convergence behavior, false: faster and do not plot cost function values)
 
 % Fix random seed
@@ -74,4 +75,5 @@ end
 % audiowrite(sprintf('%s/estimatedSignal2.wav', outputDir), sep(:,:,2), fsResample); % estimated signal 2
 
 % fprintf('The files are saved in "./output".\n');
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EOF %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
